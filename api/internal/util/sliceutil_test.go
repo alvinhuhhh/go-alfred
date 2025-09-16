a9190f942c3b6d947eccb9410e467caf83eee8ae
Alvin <alvin.tan1912@gmail.com>
2025-09-16 18:12:02 +0800

    add unit test for sliceutil

--- api/internal/util/sliceutil_test.go
+++ api/internal/util/sliceutil_test.go
@@ -1,1 +1,26 @@
 package util
+
+import (
+	"slices"
+	"testing"
+)
+
+func Test_RemoveFromSlice(t *testing.T) {
+	initial := []string{"Alice", "Bob", "Charlie", "David"}
+	expected := []string{"Alice", "Bob", "David"}
+	actual := Remove(initial, "Charlie")
+
+	if !slices.Equal(expected, actual) {
+		t.Errorf("expected and actual slices do not match")
+	}
+}
+
+func Test_RemoveFromSliceNotFound(t *testing.T) {
+	initial := []string{"Alice", "Bob", "Charlie", "David"}
+	expected := []string{"Alice", "Bob", "Charlie", "David"}
+	actual := Remove(initial, "Elaine")
+
+	if !slices.Equal(expected, actual) {
+		t.Errorf("expected and actual slices do not match")
+	}
+}
