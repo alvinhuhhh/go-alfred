928cf1f33b7b43aa5b556d227a8feb10ee94b269
Alvin <alvin.tan1912@gmail.com>
2025-09-17 23:09:06 +0800

    test telegram-apps sdk

--- ui/app/app.vue
+++ ui/app/app.vue
@@ -1,8 +1,19 @@
+<script setup>
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
+
+const { initDataRaw, initData } = retrieveLaunchParams();
+const chatId = ref("unknown");
+
+console.log(initDataRaw, initData);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Welcome to Alfred!
-    </h1>
+  <div
+    class="h-screen w-screen flex flex-col bg-slate-950 justify-center items-center"
+  >
+    <h1 class="text-3xl text-slate-50">Welcome to Alfred!</h1>
+    <body class="text-md text-slate-50">
+      Chat ID: {{ chatId }}
+    </body>
   </div>
 </template>
